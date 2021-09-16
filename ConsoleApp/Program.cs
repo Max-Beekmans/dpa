3fbf631dbea375e59fecf7765230120875fb68cf
Max Beekmans <max.beekmans@hotmail.com>
2021-09-16 12:39:22 +0200

    renamed to ConsoleApp and removed old dirs

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,12 +1,18 @@
 ﻿using System;
+using System.Collections.Generic;
+using Builder;
+using Model;
 
-namespace ConsoleApp
+namespace ConsoleTestApp
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            BodyBuilderContext context = new BodyBuilderContext();
+
+            ICelestialBody planet = context.BuildPlanet(45, 310, 0.3, -0.6, 7, "Blue");
+            ICelestialBody astroid = context.BuildAstroid(85, 56, 0, -1.5);
         }
     }
 }
